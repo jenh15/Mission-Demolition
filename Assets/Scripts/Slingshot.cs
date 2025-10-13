a13cbb8f820c520d953650c3227b7b2b116065eb
jenh15 <jhuegen@siue.edu>
2025-10-13 17:08:49 -0500

    Added Slingshot script

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
