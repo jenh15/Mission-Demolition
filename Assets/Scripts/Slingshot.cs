c9ed5c07ac7304337afa5ab9d3b0635f6f6007f9
jenh15 <jhuegen@siue.edu>
2025-10-13 17:15:27 -0500

    Updating Slingshot script

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
     void OnMouseEnter()
     {
         print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit()
     {
         print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 }
