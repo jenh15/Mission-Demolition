bfa1f1669dd8bdc47d03c2a553b693cdfa6ccf26
jenh15 <jhuegen@siue.edu>
2025-10-15 15:08:48 -0500

    Added CloudCover script

---
+++ Assets/Scripts/CloudCover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCover : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public Sprite[] clodSprites;
+    public int numClouds = 40;
+    public Vector3 minPos = new Vector3(-20, -5, -5);
+    public Vector3 maxPos = new Vector3(300, 40, 5);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
