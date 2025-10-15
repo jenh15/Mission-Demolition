f53b6ebd4512e0b611343d86a4f4ba2f73014817
JordanS490 <jordanstaggs67@gmail.com>
2025-10-15 15:57:27 -0500

    Create ProjectileLine.cs

---
+++ Assets/Scripts/ProjectileLine.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ProjectileLine: MonoBehavior {
+    private LineRenderer _line;
+    private bool _drawing = true;
+    private Projectile _projectile;
+
+    void Start(){
+        _line = GetComponent<LineRenderer>();
+        _line.positionCount = 1;
+        _line.SetPosition(0, transform.position);
+
+        _projectile = GetComponentInParent<Projectile>();
+    }
+
+    void FixedUpdate(){
+        if(_drawing) {
+            _line.positionCount++;
+            _line.SetPosition(_line.positionCount-1, transform.position);
+            if(_projectile != null){
+                _drawing = false;
+                _projectile = null;
+            }
+        }
+    }
+}
